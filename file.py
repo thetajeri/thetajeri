311a468559f7a2396c6fac1d8011dc44bc162fe4
Morteza Tajeri <thetajeri@gmail.com>
2021-07-14 23:13:12 +0430

    Version 1.1 -- we add results to a list and give theme back.

--- file.py
+++ file.py
@@ -1,4 +1,7 @@
 x = input('Please enter the status: ')
+listed = [x]
 while x.lower() != 'don':
 	print('{} is x'.format(x))
 	x = input('> ')
+	listed.append(x)
+print('\nYour list is here:\n{}'.format(listed))
