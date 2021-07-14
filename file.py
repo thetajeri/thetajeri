0dd2af74ebf9f7e8a474e14b68891773b37f14c1
Morteza Tajeri <thetajeri@gmail.com>
2021-07-15 01:21:21 +0430

    Version 2.0 -- we add process of saveing in file and a clean output.

--- file.py
+++ file.py
@@ -4,4 +4,16 @@ while x.lower() != 'don':
 	print('{} is x'.format(x))
 	x = input('> ')
 	listed.append(x)
-print('\nYour list is here:\n{}'.format(listed))
+del listed[-1]
+print('\nYour list is here:\n{}'.format('-'.join(listed)))
+
+counter = 0
+file = open('result.txt' , 'wt')
+file.write('')
+file.close()
+file = open('result.txt', 'at')
+for this_one in listed:
+	text = '{} {}\n'.format(counter,this_one)
+	file.write(text)
+	counter += 1
+file.close()
